f3893328e933c43e734a04fc9adfec6e4e9646f8
slowstate <slowstatedev@gmail.com>
2025-02-22 21:14:20 +1100

    Add crosshair

--- main.gd
+++ main.gd
@@ -4,7 +4,7 @@ extends Node2D
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	pass # Replace with function body.
+	Input.set_mouse_mode(Input.MOUSE_MODE_HIDDEN)
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
